1180b40ac11dbf27c80eb90f87c505f48bf82951
Mariano Fresno <mnofresno@gmail.com>
2018-11-17 16:55:47 -0300

    Add new strategy function feature (#2654) * Cleaned up some useless code * Improve esthetics  of the editor's screen

--- web/vue/src/components/strategyEditor/strategyEditorConfigBuilder.vue
+++ web/vue/src/components/strategyEditor/strategyEditorConfigBuilder.vue
@@ -1,10 +1,22 @@
 <template lang='pug'>
-  div
-    .hr
-    strat-picker.my2(v-on:stratConfig='updateStrat').contain
-    .hr
-    textarea.params(v-model='stratContent')
-    a.w100--s.btn--primary.scan-btn(href='#', v-on:click.prevent='save') Save file
+
+  .grd
+    .grd-row
+      .grd-row-col-5-6.px1
+        div
+          label(for='strat').wrapper Strategy:
+            .custom-select.button
+              select(v-model='strategy')
+                option(v-for='strat in strategies') {{ strat.name }}
+      .grd-row-col-5-6.px1
+        div
+          a.w100--s.btn--primary.new-btn(href='#', v-on:click.prevent='createNew') New strategy
+    .grd-row
+      .grd-row-col-5-6.px1
+        div
+          textarea(rows=20).params(v-model='stratContent')
+        div
+          a.w100--s.btn--primary.save-btn(href='#', v-on:click.prevent='save') Save file
 </template>
 
 <script>
@@ -15,136 +27,55 @@ import { get, post } from '../../tools/ajax'
 
 export default {
   created: function() {
-    get('configPart/performanceAnalyzer', (error, response) => {
-      this.performanceAnalyzer = toml.parse(response.part);
-      this.performanceAnalyzer.enabled = true;
+    get('strategies', (err, data) => {
+      this.strategies = data;
+      this.updateStrat(this.strategy);
     });
   },
   data: () => {
     return {
-      dataset: {},
+      strategies: [],
+      strategy: 'MACD',
       strat: {},
-      paperTrader: {},
-      performanceAnalyzer: {},
       stratContent: '',
     }
   },
-  components: {
-    stratPicker,
-  },
-  computed: {
-    market: function() {
-      if(!this.dataset.exchange)
-        return {};
-
-      return {
-        exchange: this.dataset.exchange,
-        currency: this.dataset.currency,
-        asset: this.dataset.asset
-      }
-    },
-    range: function() {
-      if(!this.dataset.exchange)
-        return {};
-
-      return {
-        from: this.dataset.from,
-        to: this.dataset.to
-      }
-    },
-    config: function() {
-      let config = {};
-      Object.assign(
-        config,
-        { watch: this.market },
-        { paperTrader: this.paperTrader },
-        this.strat,
-        {
-          backtest: {
-            daterange: this.range
-          },
-          backtestResultExporter: {
-            enabled: true,
-            writeToDisk: false,
-            data: {
-              stratUpdates: false,
-              roundtrips: true,
-              stratCandles: true,
-              stratCandleProps: ['open'],
-              trades: true
-            }
-          }
-        },
-        { performanceAnalyzer: this.performanceAnalyzer },
-      );
-
-      config.valid = this.validConfig(config);
-      config.backtestResultExporter.enabled = true;
-
-      return config;
+  watch: {
+    strategy: function(strat) {
+      this.strat = _.find(this.strategies, { name: strat });
+      if(!this.strat.isNew) this.updateStrat(this.strat.name);
     }
   },
   methods: {
-    validConfig: function(config) {
-      if(!config.backtest)
-        return false;
-
-      if(!config.backtest.daterange)
-        return false;
-
-      if(_.isEmpty(config.backtest.daterange))
-        return false;
-
-      if(!config.watch)
-        return false;
-
-      if(!config.tradingAdvisor)
-        return false;
-
-      let strat = config.tradingAdvisor.method;
-      if(_.isEmpty(config[ strat ]))
-        return false;
-
-      if(config.tradingAdvisor) {
-        if(_.isNaN(config.tradingAdvisor.candleSize))
-          return false;
-        else if(config.tradingAdvisor.candleSize == 0)
-          return false;
-      }
-
-      return true;
-    },
-    updateDataset: function(set) {
-      this.dataset = set;
-      this.$emit('config', this.config);
-    },
-    updateStrat: function(sc) {
-      this.strat = sc;
-      let stratName = sc.tradingAdvisor.method;
+    updateStrat: function(stratName) {
       get('strategies/' + stratName, (error, response) => {
         this.stratContent = response.content;
       });
-      this.$emit('config', this.config);
-    },
-    updatePaperTrader: function(pt) {
-      this.paperTrader = pt;
-      this.paperTrader.enabled = true;
-      this.$emit('config', this.config);
     },
     save: function() {
       let data = { content: this.stratContent };
-      let stratName = this.strat.tradingAdvisor.method;
+      let stratName = this.strat.name;
       post('strategies/' + stratName, data, (error, response) => {
+        this.strat.isNew = false;
         if(error)
           return alert(error);
-
-        this.credentials = {};
       });
-      console.log('Saving papa', this.stratContent);
+    },
+    createNew: function() {
+      let newStratName = prompt('Input new strategy name');
+      this.strategies.push({name: newStratName, isNew: true});
+      this.strategy = newStratName;
     },
   }
 }
 </script>
 
 <style>
+  .new-btn {
+    margin-top: 60px;
+    margin-bottom: 30px;
+  }
+  .save-btn {
+    margin-top: 10px;
+  }
 </style>
